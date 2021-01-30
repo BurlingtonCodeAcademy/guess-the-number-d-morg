aa525e40d79f31fab10fbb1eb7e689340f5ebc80
Dalton M <dnmorgan96@gmail.com>
2021-01-30 14:20:08 -0500

    added seperate file for reversal game

--- index.js
+++ index.js
@@ -20,7 +20,9 @@ async function start() {
   console.log(
     "Let's play a game where you (human) make up a number and I (computer) try to guess it."
   );
-  max = await ask("What do you want the high range to be? You can chose anything above 1!\n");
+  max = await ask(
+    "What do you want the high range to be? You can chose anything above 1!\n"
+  );
   max = parseInt(max); //assigns max to user input and turns that input into a number
   let secretNumber = await ask(
     "What is your secret number?\nI won't peek, I promise...\n"
@@ -31,30 +33,36 @@ async function start() {
 
   while (guess === "n" || guess === "N") {
     let nextQuestion = await ask("Higher or lower? H or L \n");
-    nextQuestion = nextQuestion.toUpperCase()
+    nextQuestion = nextQuestion.toUpperCase();
     if (nextQuestion === "L" && number <= min) {
       console.log("Nice try buster! I'm on to you. lets try this again...");
+      
       let finalGuess = await ask(`Was your number ${number}?\n`);
       if (finalGuess === "n" || finalGuess === "N") {
         console.log("You are no fun..");
         process.exit();
-      } else if (finalGuess === "y"|| finalGuess === "Y"){
-          console.log("Ha! You cheat and I still win! Best bot in the verse. Now g'day!")
-          process.exit();
-        } //Cheat detection for when the user trys to go lower than the min range
-    } else if (nextQuestion === "H" && number >= max) {
-        console.log("Nice try buster! I'm on to you. lets try this again...");
-        let finalGuess = await ask(`Was your number ${number}? \n`);
-        if (finalGuess === "n" || finalGuess === "N") {
-          console.log("You are a sore loser..");
-          process.exit();
-        }else if (finalGuess === "y"|| finalGuess === "Y"){
-          console.log("Ha! You cheat and I still win! Best bot in the verse. Now G'day!")
-          process.exit();
-        }//cheat detection for when the user trys to go higher than the max range
+      } else if (finalGuess === "y" || finalGuess === "Y") {
+        console.log(
+          "Ha! You cheat and I still win! Best bot in the verse. Now g'day!"
+        );
+        process.exit();
       }
-
-    else if (nextQuestion === "h" || nextQuestion === "H") {
+      //Cheat detection for when the user tries to go lower than the min range
+    } else if (nextQuestion === "H" && number >= max) {
+      console.log("Nice try buster! I'm on to you. lets try this again...");
+     
+      let finalGuess = await ask(`Was your number ${number}? \n`);
+      if (finalGuess === "n" || finalGuess === "N") {
+        console.log("You are a sore loser..");
+        process.exit();
+      } else if (finalGuess === "y" || finalGuess === "Y") {
+        console.log(
+          "Ha! You cheat and I still win! Best bot in the verse. Now G'day!"
+        );
+        process.exit();
+      }
+      //cheat detection for when the user tries to go higher than the max range
+    } else if (nextQuestion === "h" || nextQuestion === "H") {
       min = number + 1;
       number = educatedGuess(min, max);
       guess = await ask(`Was your number ${number}? Y or N \n`);
@@ -63,12 +71,13 @@ async function start() {
       number = educatedGuess(min, max);
       guess = await ask(`Was your number ${number}? Y or N \n`);
     }
-    
-  } //while loop to reiterate through the question process with modified variables based on user input
+  }
+  //while loop to reiterate through the question process with modified variables based on user input
   if (guess === "y" || guess === "Y") {
     console.log(
       "I win silly human!! Ha! I am the greatest bot in the multiverse"
     );
     process.exit();
-  } //final if statement for when the correct number is guessed. computer gloats and quits.
+  }
+  //final if statement for when the correct number is guessed. computer gloats and quits.
 }
