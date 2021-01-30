e59ef25514be92a91ceb85c63a7c8ae72c9452df
Dalton M <dnmorgan96@gmail.com>
2021-01-30 11:56:46 -0500

    added the cheat detection function in the while loop

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ async function start() {
   console.log(
     "Let's play a game where you (human) make up a number and I (computer) try to guess it."
   );
-  max = await ask("What do you want the high range to be?  50 - 100 \n");
+  max = await ask("What do you want the high range to be? You can chose anything above 1!\n");
   max = parseInt(max); //assigns max to user input and turns that input into a number
   let secretNumber = await ask(
     "What is your secret number?\nI won't peek, I promise...\n"
@@ -31,7 +31,30 @@ async function start() {
 
   while (guess === "n" || guess === "N") {
     let nextQuestion = await ask("Higher or lower? H or L \n");
-    if (nextQuestion === "h" || nextQuestion === "H") {
+    nextQuestion = nextQuestion.toUpperCase()
+    if (nextQuestion === "L" && number <= min) {
+      console.log("Nice try buster! I'm on to you. lets try this again...");
+      let finalGuess = await ask(`Was your number ${number}?\n`);
+      if (finalGuess === "n" || finalGuess === "N") {
+        console.log("You are no fun..");
+        process.exit();
+      } else if (finalGuess === "y"|| finalGuess === "Y"){
+          console.log("Ha! You cheat and I still win! Best bot in the verse. Now g'day!")
+          process.exit();
+        } //Cheat detection for when the user trys to go lower than the min range
+    } else if (nextQuestion === "H" && number >= max) {
+        console.log("Nice try buster! I'm on to you. lets try this again...");
+        let finalGuess = await ask(`Was your number ${number}? \n`);
+        if (finalGuess === "n" || finalGuess === "N") {
+          console.log("You are a sore loser..");
+          process.exit();
+        }else if (finalGuess === "y"|| finalGuess === "Y"){
+          console.log("Ha! You cheat and I still win! Best bot in the verse. Now G'day!")
+          process.exit();
+        }//cheat detection for when the user trys to go higher than the max range
+      }
+
+    else if (nextQuestion === "h" || nextQuestion === "H") {
       min = number + 1;
       number = educatedGuess(min, max);
       guess = await ask(`Was your number ${number}? Y or N \n`);
@@ -40,6 +63,7 @@ async function start() {
       number = educatedGuess(min, max);
       guess = await ask(`Was your number ${number}? Y or N \n`);
     }
+    
   } //while loop to reiterate through the question process with modified variables based on user input
   if (guess === "y" || guess === "Y") {
     console.log(
