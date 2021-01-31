4d97a0d293b29bd447b09ace42fda84725c0ed47
Dalton M <dnmorgan96@gmail.com>
2021-01-31 13:32:32 -0500

    Added comments, cleaned some things up and added some variables for counters, changed the auto re start feature to a await ask function as well

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ start();
 
 let min = 1;
 let max = undefined; //leaving undefined in order to allow user input
+let guessCounter = 1;
+let userGuessCounter = 0; //added counter variables to count inputs.
 
 function ask(questionText) {
   return new Promise((resolve, reject) => {
@@ -22,22 +24,23 @@ function randomInteger(min, max) {
 } //function to allow computer to pick a random number
 
 async function start() {
-  console.log("Hello, would you want to do the guessing or shall I?\n");
+  console.log("\n\nHello, would you want to do the guessing or shall I?\n");
   let gameSelection = await ask(
     "Type 'you' for computer.\nOr type 'me' for yourself!\n"
   );
   gameSelection = gameSelection.toUpperCase();
   if (gameSelection === "ME") {
-    let computerNumber = randomInteger(0, 100);
+    let computerNumber = randomInteger(1, 100);
 
     console.log(
-      "lets play a game where I (computer) come up with a number and you (human) try to guess it!"
+      "\nlets play a game where I (computer) come up with a number between 1 - 100 and you (human) try to guess it!"
     );
 
     let humanGuess = await ask("Okay I'm ready, try to guess my number!\n");
     humanGuess = parseInt(humanGuess);
 
     while (humanGuess !== computerNumber) {
+      userGuessCounter++;
       humanGuess = parseInt(humanGuess); //reassigning variable so it needs to be converted again
       if (humanGuess > computerNumber) {
         humanGuess = await ask(
@@ -47,32 +50,40 @@ async function start() {
         humanGuess = await ask("Nope! guess higher you fool\n");
       }
       if (humanGuess === computerNumber) {
-        console.log(`Correct! My number was ${humanGuess}`);
-        start();
+        let playAgain = await ask(
+          `\nCorrect! My number was ${humanGuess}\n\nIt took you ${userGuessCounter} tries!\n\nPlay again? 'Y' or 'N'\n`
+        );
+        playAgain = playAgain.toUpperCase();
+        if (playAgain === "Y") {
+          userGuessCounter = 1;
+          start();
+        } else process.exit();
       } //while loop to reiterate the asking process until the user guesses the correct number.
     }
   } else if (gameSelection === "YOU") {
     console.log(
-      "Let's play a game where you (human) make up a number and I (computer) try to guess it."
+      "\nLet's play a game where you (human) make up a number and I (computer) try to guess it."
     );
     max = await ask(
       "What do you want the high range to be? You can chose anything above 1!\n"
     );
+    console.log(`You set the high range to be: ${max}\n`);
     max = parseInt(max); //assigns max to user input and turns that input into a number
     let secretNumber = await ask(
       "What is your secret number?\nI won't peek, I promise...\n"
     );
-    console.log("You entered: " + secretNumber);
+    console.log(`You entered: ${secretNumber} \n`);
     number = educatedGuess(min, max);
     let guess = await ask(`Was your number ${number}? Y or N \n`);
 
     while (guess === "n" || guess === "N") {
+      guessCounter++;
       let nextQuestion = await ask("Higher or lower? H or L \n");
       nextQuestion = nextQuestion.toUpperCase();
       if (nextQuestion === "L" && number <= min) {
         console.log("Nice try buster! I'm on to you. lets try this again...");
 
-        let finalGuess = await ask(`Was your number ${number}?\n`);
+        let finalGuess = await ask(`Was your number ${number}? 'Y' or 'N'\n`);
         if (finalGuess === "n" || finalGuess === "N") {
           console.log("You are no fun..");
           process.exit();
@@ -82,11 +93,11 @@ async function start() {
           );
           process.exit();
         }
-        //Cheat detection for when the user tries to go lower than the min range
+        //^Cheat detection for when the user tries to go lower than the min range^
       } else if (nextQuestion === "H" && number >= max) {
         console.log("Nice try buster! I'm on to you. lets try this again...");
 
-        let finalGuess = await ask(`Was your number ${number}? \n`);
+        let finalGuess = await ask(`Was your number ${number}? 'Y' or 'N' \n`);
         if (finalGuess === "n" || finalGuess === "N") {
           console.log("You are a sore loser..");
           process.exit();
@@ -96,26 +107,32 @@ async function start() {
           );
           process.exit();
         }
-        //cheat detection for when the user tries to go higher than the max range
+        //^cheat detection for when the user tries to go higher than the max range^
       } else if (nextQuestion === "h" || nextQuestion === "H") {
-        min = number + 1;
+        min = number+1;
         number = educatedGuess(min, max);
         guess = await ask(`Was your number ${number}? Y or N \n`);
       } else if (nextQuestion === "l" || nextQuestion === "L") {
-        max = number - 1;
+        max = number-1;
         number = educatedGuess(min, max);
         guess = await ask(`Was your number ${number}? Y or N \n`);
-      }
+      } //Asking user high or lower to reassign ends of the range.
     }
-    //while loop to reiterate through the question process with modified variables based on user input
+    //^while loop to reiterate through the question process with modified variables based on user input^
     if (guess === "y" || guess === "Y") {
-      console.log(
-        "I win silly human!! Ha! I am the greatest bot in the multiverse"
+      let playAgain = await ask(
+        `\nI win silly human!! Ha! I am the greatest bot in the multiverse!\n\nIt only took me ${guessCounter} tries!\n\nPlay again? 'Y' or 'N'\n`
       );
-      start();
-    } //final if statement for when the correct number is guessed. computer gloats and quits.
+      playAgain = playAgain.toUpperCase();
+      if (playAgain === "Y") {
+        max = undefined;
+        min = 1;
+        guessCounter = 1; //reassigning variables so it starts fresh at beginning of program
+        start();
+      } else process.exit();
+    } //^final if statement with another else for when the correct number is guessed. computer gloats and inquires for another round.^
   } else {
     console.log("hmm this doesn't look quite right...");
-    start();
+    start(); //^a catch all for if an unrecognized input is entered. program will restart. ^
   }
 }
